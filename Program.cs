882b3f862269aa0e268f79541a39124306c299e7
diego_neri <diego@diegoneri.com>
2020-10-02 20:11:22 -0300

    Finaliza o exercicio Cebolinha

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem;
+
+            Console.WriteLine("Olá. Digite sua mensagem: ");
+            mensagem = Console.ReadLine();
+
+            mensagem = mensagem.Replace("rr", "l")
+                               .Replace("RR", "L")
+                               .Replace("rR", "l")
+                               .Replace("Rr", "L")
+                               .Replace("R", "L")
+                               .Replace("r", "l");
+            
+            //mensagem = mensagem.Replace("R", "L");
+
+            Console.WriteLine(mensagem);
+        }
+    }
+}
